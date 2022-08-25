d67bb5d059d13278acc7a08bf734bf90dd4fe8f1
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-25 19:12:11 -0400

    Created about me component with jsx

---
+++ src/components/about/about.js
@@ -0,0 +1,31 @@
+
+
+function About() {
+    return(
+        <main>
+            <p>A diligent Front-End focused Web Developer with 
+                2+ years of experience. 
+            </p>
+
+            <p>Collobarated with solo entrepreneurs and businesses
+               to transform their digital presence by building 
+               stunning, highly intuituve and performant custom web apps
+
+            </p>
+
+            <p>
+                My primary tech stack in my toolbelt is HTML5, CSS3, React.js, 
+                Javascript, Bootstrap, Node.js, Express.Js etc.
+            </p>
+
+            <p> I always follow best practices 
+                and current tools
+            </p>
+
+          
+        </main>
+    )
+}
+
+
+export default About;
