d84b4c7d9a6968a2a10a39557e59f5134f34a6a6
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-30 14:41:11 -0400

    Modified hero section to make it more minimalistic

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,38 +4,38 @@ import githubIcon from '../../assets/icons/github.svg';
 import contactIcon from '../../assets/icons/contact.png';
 import downloadIcon from '../../assets/icons/download.png';
 import scrolldown from '../../assets/icons/scrolldown.gif';
+import wavyLine from '../../assets/icons/wavy-pink.png';
 import './hero.scss';
 
 function Hero() {
     return(
         <main className='hero' >
-            <section className='hero__image-box' >
-                <img src={heroImage} className='hero__image' />
-            </section>
+            <section className='hero__about' >
 
-            <section className='hero__title-box' >
-                <h1 className='hero__title' >VICTORY-RITA ONUOHA</h1>
-                <h2 className='hero__job-title'> 💻 Front End Developer</h2>
-            </section>
+                <article className='hero__user' >
+                    <section className='hero__user-name' >
+                        <img src={wavyLine} className='hero__user-line'/>
+                        <p className='hero__user-title' >Victory-Rita Onuoha</p>
+                    </section>
 
-            <section className='hero__description-box' >
-                <h2 className='hero__description' > A specialist in creating 
-                </h2>
-                <span className='hero__description--selected'> Intuitive, Pixel-perfect,
-                    Responsive and Optimized web apps. ⭐</span>
-            </section>
+                    <section className='hero__title-box' >
+                        <h2 className='hero__job-title'> Hello, my name is Victory.</h2>
+                        <h2 className='hero__job-title'> I am a Creative Web Developer.</h2>
+                    </section>
+                </article>
+            
+                
+                <article className='hero__user-description' >
+                    <section className='hero__link-box' >
+                        <img alt='logo and link for linkledn page' src={linkedinIcon} className='hero__linkedin-link' />
+                        <img alt='logo and link for github' src={githubIcon} className='hero__github-link' />
+                    </section>
 
-            <section className='hero__link-box' >
-                <img alt='logo and link for linkledn page' src={linkedinIcon} className='hero__linkedin-link' />
-                <img alt='logo and link for github' src={githubIcon} className='hero__github-link' />
+                </article>
             </section>
-
+            
+           
             <section className='hero__buttons' >
-                <button className='hero__button' >
-                    Work with me
-                    <img src={contactIcon} alt=' message us icon' className='hero__contact-button' />
-                </button>
-
                 <button className='hero__button'>
                     Download CV
                     <img src={downloadIcon} alt=' download cv icon' className='hero__download-button'/>
