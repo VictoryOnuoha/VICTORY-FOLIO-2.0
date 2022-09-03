c614c06e13f79c59c2a958cc8229b4457629397b
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-09-03 15:10:27 -0400

    Created data file for project card, content will be modified later

--- src/assets/data/card.js
+++ src/assets/data/card.js
@@ -1,46 +1,67 @@
+import card1 from '../../assets/images/card1.jpg';
+import card2 from '../../assets/images/card2.jpg';
+import card3 from '../../assets/images/card3.jpg';
+import card4 from '../../assets/images/card4.jpg';
+import card5 from '../../assets/images/card5.jpg';
+import card6 from '../../assets/images/card6.jpg';
+import card7 from '../../assets/images/card7.jpg';
 
 const cards = [
     {
         title: 'VICTORY FOLIO',
         category: ['WEB APP', 'FEATURED'],
         description: 'Lorem ipsum dolro sit amt, consestcuer elit',
-        image: '',
-        id: 1
+        image: card1,
+        id: '2629f55a-2bb4-11ed-a261-0242ac120002'
 
     },
     {
         title: 'IN WHOLENESS PRACTICE',
         category: 'WEB APP',
         description: 'Lorem ipsum dolro sit amt, consestcuer elit',
-        image: '',
-        id: 2
+        image: card2,
+        id: '2629f71c-2bb4-11ed-a261-0242ac120002'
     },
     {
         title: 'TRIBE HAIRCARE',
         category: ['WEB APP', 'MOBILE APP', 'FEATURED'],
         description: 'Lorem ipsum dolro sit amt, consestcuer elit',
-        image: '',
-        id: 3
+        image: card3,
+        id: '2629f80c-2bb4-11ed-a261-0242ac120002'
     },
     {
         title: 'TRIBE SKINCARE',
         category: ['WEB APP', 'MOBILE APP'],
         description: 'Lorem ipsum dolro sit amt, consestcuer elit',
-        image: '',
-        id: 4
+        image: card4,
+        id: '2629f8e8-2bb4-11ed-a261-0242ac120002'
     },
     {
-        title: 'BUG TRACKER',
+        title: 'SPRINTDAY',
         category: 'WEB APP',
         description: 'Lorem ipsum dolro sit amt, consestcuer elit',
-        image: '',
-        id: 5
+        image: card5,
+        id: '2629fb04-2bb4-11ed-a261-0242ac120002'
     },
     {
         title: 'PATIENT PORTAL',
         category: 'MOBILE APP',
         description: 'Lorem ipsum dolro sit amt, consestcuer elit',
-        image: '',
-        id: 6
+        image: card6,
+        id: '2629fbea-2bb4-11ed-a261-0242ac120002'
+    },
+    {
+        title: 'MICHELLE DOVER ARTIST FOLIO',
+        category: 'WEB APP',
+        description: 'Lorem ipsum dolro sit amt, consestcuer elit',
+        image: card7,
+        id: '2629fdc0-2bb4-11ed-a261-0242ac120002'
+    },
+    {
+        title: 'SEBUM',
+        category: 'WEB APP',
+        description: 'Lorem ipsum dolro sit amt, consestcuer elit',
+        image: card6,
+        id: '2629feb0-2bb4-11ed-a261-0242ac120002'
     }
 ]
