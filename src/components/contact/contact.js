62baa0b3f3f0ad2d2ef1c103bf7a212337aa56c5
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-09-08 14:32:24 -0400

    Added styling for contact section for mobile and tablet queries

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-
+import './contact.scss';
 
 function Contact() {
 
@@ -11,51 +11,64 @@ function Contact() {
 
 
     return(
-        <main>
-            <section>
-                <h3>LET'S CHAT</h3>
+        <main className='contact' >
+            <section className='contact__title-box' >
+                <h3 className='contact__title' >LET'S CHAT</h3>
             </section>
 
-            <section>
-                <form>
-                    <label for='fname' >
+            <section className='contact__form-box' >
+                <form
+                className='contact__form'
+                >
+                    <label for='fname' className='contact__name' >
+                        Full Name
                         <input 
                           id='fname'
                           type='text'
                           value=''
                           name=''
-                          placeholder='First and last name is..' />
+                          placeholder='First and last name is..'
+                          className='contact__input' />
                     </label>
+                    <br/>
+                  
 
-                    <label for='email'>
+                    <label for='email' className='contact__email'>
+                        Email
                         <input
                           id='email'
                           type='email'
                           value=''
                           name=''
-                          placeholder='example@live.com'/>
+                          placeholder='example@live.com'
+                          className='contact__input'/>
                     </label>
-                    <label for='customer'>
+                    <br/>
+                    <label for='customer' className='contact__customer'>
                         I am a/ an 
+                        <br/>
                         <select 
                           id='customer'
                           type='text'
-                          value='' >
+                          value=''
+                          className='contact__input' >
                             <option >Recruiter</option>
                             <option>Business Owner</option>
                             <option>Press/ Media</option>
                         </select>
                     </label> 
-
-                    <label for='message'>
+                    <br/>
+                    <label for='message' className='contact__message' >
+                        Message
                         <textarea
                             id='message'
                             name='message'
                             value=''
-                            placeholder='I want to work with you'/>
+                            placeholder='I want to work with you'
+                            className='contact__input contact__message-box'/>
                     </label>
-                    <section>
-                      <input type='submit' name='submit' value="Submit"/>
+                    <section className='contact__button-box'>
+                      <input type='submit' name='submit' value="CONTACT ME" className='contact__button'/>
                     </section>
                 </form>
             </section>
