72f7ce691a9451eca486c3138f47a0808f5f6931
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2023-01-08 23:42:25 -0500

    modified portfolio styling and image and updated links

--- src/assets/data/card.js
+++ src/assets/data/card.js
@@ -1,17 +1,17 @@
-import card1 from '../../assets/images/card1.png';
-import card2 from '../../assets/images/card2.png';
-import card4 from '../../assets/images/card4.png';
-import card7 from '../../assets/images/card7.png';
+import card1 from '../../assets/images/card 1.png';
+import card2 from '../../assets/images/card 2.png';
+import card4 from '../../assets/images/card 4.png';
+import card7 from '../../assets/images/card 7.png';
 
 const cards = [
     {
-        title: 'VICTORY FOLIO',
+        title: 'IN WHOLENESS',
         category: ['WEB APP', 'FEATURED', 'WEBSITE'],
-        description: 'Epic and minimal portfolio website template for tech profesionals',
+        description: 'A dynamic and responsive multi page website for the expert nutritionist',
         image: card1,
-        live: 'https://victoryonuoha.com',
-        repo: 'https://github.com/VictoryOnuoha/VICTORY-FOLIO-2.0',
-        technology: 'React JS, Javascript, SASS, Email JS, Node JS, Firebase',
+        live: 'https://in-wholeness.web.app/',
+        repo: 'https://github.com/RijeStudios/in-wholeness',
+        technology: 'React JS, Javascript, SASS, Slider JS, Node JS, Firebase',
         id: '2629f55a-2bb4-11ed-a261-0242ac120002'
 
     },
@@ -20,7 +20,7 @@ const cards = [
         category: ['WEB APP', 'FEATURED', 'WEBSITE'],
         description: 'A stunning and responsive landing page for the best coffee in the world!',
         image: card2,
-        live: 'https://retro-coffee-shop.herokuapp.com/index.html',
+        live: 'https://victoryonuoha.github.io/retro-coffee-shop/index.html',
         repo: 'https://github.com/VictoryOnuoha/retro-coffee-shop',
         technology: 'HTML5, SASS',
         id: '2629f71c-2bb4-11ed-a261-0242ac120002'
@@ -30,19 +30,19 @@ const cards = [
         category: ['WEB APP', 'FEATURED', 'WEBSITE'],
         description: 'Trendy and dynamic website for the rock n roll icon',
         image: card4,
-        live: 'https://bandsite-rock-icon.herokuapp.com/index.html',
+        live: 'https://victoryonuoha.github.io/bandsite/index.html',
         repo: 'https://github.com/VictoryOnuoha/bandsite',
-        technology: 'React JS, Javascript, SASS, Rest API',
+        technology: 'React JS, Javascript, SASS, REST API',
         id: '2629f8e8-2bb4-11ed-a261-0242ac120002'
     },
     {
-        title: 'MEDICAL DOCTOR WEBSITE',
+        title: 'VICTORY FOLIO',
         category: ['WEB APP', 'WEBSITE'],
-        description: 'Elegant and SEO friendly landing page for the busy medical professional',
+        description: 'Elegant and SEO friendly lwebsite for the professional software engineer',
         image: card7,
-        live: 'https://medical-expert.herokuapp.com/',
-        repo: 'https://github.com/VictoryOnuoha/expert-template',
-        technology: 'React JS, Javascript, SASS, Node JS',
+        live: 'https://victoryonuoha.com/',
+        repo: 'https://github.com/VictoryOnuoha/VICTORY-FOLIO-2.0',
+        technology: 'React JS, Javascript, SASS, Node JS, Firebase',
         id: 'bfeb748e-3131-11ed-a261-0242ac120002'
     },
 
--- src/components/about/aboutme.js
+++ src/components/about/aboutme.js
@@ -5,82 +5,80 @@ import interests from '../../assets/images/interests.svg';
 import resume from '../../assets/files/Victory_Onuoha_Resume_Front_End_Software_Developer.pdf';
 
 function About() {
-    return(
+    return (
         <main className='about' id='about' >
-        
 
-            <section className='about__me-box' >
+
+            
                 <article className='about__title-box' >
                     {/* <img src={wavyWhite} className='about__title-line' alt='a decorative line'/> */}
-                    <h2 className='about__title'>ABOUT ME </h2>
-                </article>   
-            
-                    {/* <h1 className='about__info' >Hello, World 🦋
-                    </h1> */}
-            </section>
+                    <h2 className='about__title'>ABOUT ME 👋🏾 </h2>
+                </article>
 
+                {/* about user=============== */}
             <section className='about__user' >
                 <article className='about__text-box' >
-                    <p className='about__text' >Hi, my name is Victory Onuoha. Welcome to my personal site 👋🏾
-                    </p>
-                    
-                    <p className='about__text' >I love all things technology, productivity and 
-                     nature related. Have fun exploring! 
+                    <p className='about__text' >Hi, my name is Victory Onuoha. 
                     </p>
 
-                  
+                    <p className='about__text' >I love all things technology, productivity and
+                        nature related. Have fun exploring!
+                    </p>
+
+
                 </article>
                 <section className='about__photo-box'>
-                    <img src={headshot} alt='Victory' className='about__photo'  />
+                    <img src={headshot} alt='Victory' className='about__photo' />
                 </section>
             </section>
 
             <article className='about__skills-box' >
-                    <h2 className='about__title' >BACKGROUND 💻</h2>
+                <h2 className='about__title' >BACKGROUND 💻</h2>
             </article>
 
-            <section>
-                <article className='about__user' >
+            {/* about background=============== */}
+            <section className='about__user'>
+              
                     <section className='about__photo-box'>
                         <img src={background} alt='Victory' className='about__photo' />
                     </section>
 
-                    <section className='about__text-box' >
+                    <article className='about__text-box' >
                         <p className='about__text'>
                             Software Engineer with additional experience in healthcare & operations.
                         </p>
                         <p className='about__text' >
-                            I have Bachelors in Neuroscience and a diploma in Web Development.
+                            I have a Bachelors of Science in Neuroscience and a diploma in Web Development.
                             I am passionate about using technolgy to solve client-facing problems.
                         </p>
-                    </section>
-
-                </article>
+                    </article>
 
             </section>
 
             <article className='about__skills-box' >
-                    <h2 className='about__title' > INTERESTS 🤹🏽</h2>
+                <h2 className='about__title' > INTERESTS 🤹🏽</h2>
             </article>
-            
+
+            {/* about interests=============== */}
             <section className='about__user'>
                 <article className='about__text-box' >
                     <p className='about__text'>
-                    In my spare time, I create and experiment with web and app development.
-                     Some of which can be found on Github.
+                        In my spare time, I create and experiment with web and app development.
+                        Some of which can be found on Github.
                     </p>
                     <p className='about__text' >
-                    My other interests include personal finance, Artifical Intelligence, automation, reading, writing, 
-                    design, emerging technologies and pop culture.
+                        My other interests include personal finance, artifical intelligence, automation, reading, writing,
+                        design, emerging technologies and pop culture.
                     </p>
-                       
+
                 </article>
                 <section className='about__photo-box'>
-                        <img src={interests} alt='Victory' className='about__photo'  />
+                    <img src={interests} alt='Victory' className='about__photo' />
                 </section>
-                
+
             </section>
-            <section>
+
+            
                 {/* <section className='about__skills-box' >
                     <h2 className='about__skills' >SKILLS 💻</h2>
                 </section>
@@ -106,14 +104,14 @@ function About() {
 
                 </ul> */}
 
-            <section className='about__buttons' >
-                
+                <section className='about__buttons' >
+
                     <a className='about__button' target="_blank" href={resume} download>
-                     DOWNLOAD CV
+                        DOWNLOAD CV
                     </a>
-               
-            </section>
-        </section> 
+
+                </section>
+           
         </main>
     )
 }
