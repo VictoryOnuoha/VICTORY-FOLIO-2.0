cf82b0fc95a36b9599abd1ebe3a2e036001de0c2
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2023-03-02 23:20:46 -0500

    modified project card image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/header/header'; 
 import About from './components/about/about';
 import Hero from './components/hero/heros';
-import Projects from './components/projects/projects';
+import Projects from './components/projects/project';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
